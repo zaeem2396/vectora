28150edd6b161d71a063c8433a5b0b13b4ac2cbf
cod3xa <zaeem@nagels.tech>
2026-03-27 10:44:12 +0530

    test(unit): add ApiErrorCategory status code mapping tests

---
+++ tests/Unit/Core/ApiErrorCategoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vectora\Pinecone\Tests\Unit\Core;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Vectora\Pinecone\Core\Exception\ApiErrorCategory;
+
+final class ApiErrorCategoryTest extends TestCase
+{
+    /**
+     * @return iterable<string, array{int, ApiErrorCategory}>
+     */
+    public static function statusCodeProvider(): iterable
+    {
+        yield '429' => [429, ApiErrorCategory::RateLimited];
+        yield '401' => [401, ApiErrorCategory::Authentication];
+        yield '403' => [403, ApiErrorCategory::Authentication];
+        yield '404' => [404, ApiErrorCategory::NotFound];
+        yield '400' => [400, ApiErrorCategory::BadRequest];
+        yield '422' => [422, ApiErrorCategory::BadRequest];
+        yield '418' => [418, ApiErrorCategory::Client];
+        yield '500' => [500, ApiErrorCategory::Server];
+        yield '599' => [599, ApiErrorCategory::Server];
+    }
+
+    #[DataProvider('statusCodeProvider')]
+    public function test_from_status_code_maps_expected_category(int $code, ApiErrorCategory $expected): void
+    {
+        $this->assertSame($expected, ApiErrorCategory::fromStatusCode($code));
+    }
